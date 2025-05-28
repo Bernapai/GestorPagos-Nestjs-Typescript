0e7d00bd7af0873526ea3862543f5b81da93727f
Bernapai <juanemeunier@gmail.com>
2025-05-27 22:13:10 -0300

    Set New Setting for eslint + Configuration for Main.ts

--- Microservices/user-service/src/main.ts
+++ Microservices/user-service/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 3004,
+      },
+    },
+  );
+
+  // Añade esto para habilitar la validación
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  console.log('Microservicio escuchando en el puerto 3004');
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error('Error during bootstrap:', error);
+});
