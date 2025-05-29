7745f195179f2f084d420857cfb1ba8529709817
Bernapai <juanemeunier@gmail.com>
2025-05-28 21:24:09 -0300

    Add user Entity and Dtos

---
+++ Microservices/user-service/src/user/user.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users') // nombre de la tabla
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
