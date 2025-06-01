81fd5ac4941674c91f1681b2b5c2ec56aabe4a57
Bernapai <juanemeunier@gmail.com>
2025-06-01 20:09:45 -0300

    Add usersServices

--- Microservices/notification-service/src/notification/dto/createNotification.dto.ts
+++ Microservices/notification-service/src/notification/dto/createNotification.dto.ts
@@ -1,7 +1,7 @@
 import { IsString, IsPhoneNumber } from 'class-validator';
 
 export class SendSmsDto {
-  @IsPhoneNumber(undefined) // acepta cualquier país (podés usar 'AR' o 'US' si querés limitar)
+  @IsPhoneNumber(undefined)
   to: string;
 
   @IsString()
--- Microservices/user-service/src/user/user.service.ts
+++ Microservices/user-service/src/user/user.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UpdateUserDto } from './dto/updateUser.dto';
 @Injectable()
-export class UserService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({
+      id: id,
+    });
+    if (!user) {
+      throw new Error(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    await this.userRepository.update(id, updateUserDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ email });
+  }
+
+  async findByUsername(name: string): Promise<User | null> {
+    return this.userRepository.findOneBy({ name });
+  }
+}
