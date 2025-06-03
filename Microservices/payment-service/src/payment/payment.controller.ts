f6a84f1e83ee7596a9be84717f1a1409d059b5b0
Bernapai <juanemeunier@gmail.com>
2025-06-03 18:27:46 -0300

    Add paymentController

--- Microservices/payment-service/src/payment/payment.controller.ts
+++ Microservices/payment-service/src/payment/payment.controller.ts
@@ -1,7 +1,40 @@
 import { Controller } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { CreatePaymentDto } from './dto/createPayment.dto';
+import { UpdatePaymentDto } from './dto/updatePayment.dto';
+import { Payment } from './payment.entity';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('payment')
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
+  constructor(private readonly paymentService: PaymentService) { }
+
+  @MessagePattern({ cmd: 'create-payment' })
+  async create(
+    @Payload() createPaymentDto: CreatePaymentDto,
+  ): Promise<Payment> {
+    return this.paymentService.create(createPaymentDto);
+  }
+
+  @MessagePattern({ cmd: 'get-payments' })
+  async findAll(): Promise<Payment[]> {
+    return this.paymentService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'get-payment' })
+  async findOne(@Payload() id: number): Promise<Payment> {
+    return this.paymentService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'update-payment' })
+  async update(
+    @Payload() payload: { id: number; dto: UpdatePaymentDto },
+  ): Promise<Payment> {
+    return this.paymentService.update(payload.id, payload.dto);
+  }
+
+  @MessagePattern({ cmd: 'delete-payment' })
+  async remove(@Payload() id: number): Promise<void> {
+    return this.paymentService.remove(id);
+  }
 }
