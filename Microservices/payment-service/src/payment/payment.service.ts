b703c0ded570c6aee2ff90b32ec18e9c5a726b2d
Bernapai <juanemeunier@gmail.com>
2025-06-03 18:26:04 -0300

    Add paymentServices + UpdatePaymentDtos

--- Microservices/payment-service/src/payment/payment.service.ts
+++ Microservices/payment-service/src/payment/payment.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePaymentDto } from './dto/createPayment.dto';
+import { UpdatePaymentDto } from './dto/updatePayment.dto';
+import { Payment } from './payment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+  ) { }
+
+  async create(createPaymentDto: CreatePaymentDto): Promise<Payment> {
+    const payment = this.paymentRepository.create(createPaymentDto);
+    return this.paymentRepository.save(payment);
+  }
+
+  async findAll(): Promise<Payment[]> {
+    return this.paymentRepository.find();
+  }
+
+  async findOne(id: number): Promise<Payment> {
+    const payment = await this.paymentRepository.findOneBy({ id });
+    if (!payment) {
+      throw new Error(`Payment with id ${id} not found`);
+    }
+    return payment;
+  }
+
+  async update(
+    id: number,
+    updatePaymentDto: UpdatePaymentDto,
+  ): Promise<Payment> {
+    await this.paymentRepository.update(id, updatePaymentDto);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.paymentRepository.delete(id);
+  }
+}
