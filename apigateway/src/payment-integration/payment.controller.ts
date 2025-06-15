44a3e89bc7e31889a286dd6afa0602782ae305b8
Bernapai <juanemeunier@gmail.com>
2025-06-15 19:31:05 -0300

    Add PymentController from ApiGateway Integration

---
+++ apigateway/src/payment-integration/payment.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Body,
+  Patch,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto } from '../../../Microservices/payment-service/src/payment/dto/createPayment.dto';
+import { UpdatePaymentDto } from '../../../Microservices/payment-service/src/payment/dto/updatePayment.dto';
+
+@Controller('payments')
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) { }
+
+  @Post()
+  create(@Body() dto: CreatePaymentDto) {
+    return this.paymentService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.paymentService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.paymentService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdatePaymentDto) {
+    return this.paymentService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.paymentService.remove(id);
+  }
+}
